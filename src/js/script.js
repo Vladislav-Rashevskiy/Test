43792bcdb611f5af768a90d231e09db44d45d614
Vladislav <vladislav.rashevskiy@gmail.com>
2021-02-04 17:17:31 +0300

    added adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
-const test = [1,2,3]
+// Решил написать на нативном, но так же знаю jQuery.
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".header-navbar__menu");
+  const burgerButton = document.querySelector(".header-navbar__btn");
 
+  burgerButton.addEventListener("click", () => {
+    if (menu.classList.contains("active")) {
+      menu.classList.remove("active");
+      burgerButton.classList.remove("active");
+    } else {
+      menu.classList.add("active");
+      burgerButton.classList.add("active");
+    }
+  });
+});
